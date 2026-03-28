ba4afe6a6edaa76945e669a109dd1a0ce717a783
Christian <cedenoc181@gmail.com>
2026-03-28 13:18:58 -0400

    updating naming convention from scout ai to scouting report

--- src/components/pages/Stats/mlb-schedule/components/ScoutAiButton.jsx
+++ src/components/pages/Stats/mlb-schedule/components/ScoutAiButton.jsx
@@ -18,7 +18,7 @@ export default function ScoutAiButton({ hasAnalysis, loading, onClick, isToday =
             <path d="M12 2a5 5 0 0 1 5 5c0 1.5-.67 2.83-1.72 3.75L17 22H7l1.72-11.25A5 5 0 0 1 12 2z"/>
             <line x1="9" y1="9" x2="15" y2="9"/>
           </svg>
-          <span>View Scout Analysis</span>
+          <span>Scouting Report</span>
         </>
       ) : (
         <>
@@ -26,7 +26,7 @@ export default function ScoutAiButton({ hasAnalysis, loading, onClick, isToday =
             <path d="M9.5 2A2.5 2.5 0 0 1 12 4.5v15a2.5 2.5 0 0 1-4.96-.46 2.5 2.5 0 0 1-2.96-3.08 3 3 0 0 1-.34-5.58 2.5 2.5 0 0 1 1.32-4.24 2.5 2.5 0 0 1 4.44-1.14Z"/>
             <path d="M14.5 2A2.5 2.5 0 0 0 12 4.5v15a2.5 2.5 0 0 0 4.96-.46 2.5 2.5 0 0 0 2.96-3.08 3 3 0 0 0 .34-5.58 2.5 2.5 0 0 0-1.32-4.24 2.5 2.5 0 0 0-4.44-1.14Z"/>
           </svg>
-          <span>Scout AI</span>
+          <span>Scouting Report</span>
         </>
       )}
     </button>
